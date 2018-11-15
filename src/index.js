c3ae8cc0de8668ff69cc267b98eca59069fdea40
Michelle Beshears <meeshysu@gmail.com>
2018-11-14 19:53:15 -0600

    console log working for auth

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
 import firebase from 'firebase/app';
 
 import apiKeys from '../db/apiKeys.json';
+import createNavBar from './components/Navbar/navbar';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
-  $('#list').html('this prints');
+  createNavBar();
 };
 initializeApp();
