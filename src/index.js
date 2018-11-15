fc91c05f7ab417e30d583a52957db7090a79fb51
Michelle Beshears <meeshysu@gmail.com>
2018-11-14 19:07:14 -0600

    firebase project setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
+import firebase from 'firebase/app';
 
-console.log('hi');
+import apiKeys from '../db/apiKeys.json';
+
+const initializeApp = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  $('#list').html('this prints');
+};
+initializeApp();
