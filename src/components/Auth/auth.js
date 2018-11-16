64d347beec30af7cf20ff560082d8dd22fd23852
Michelle Beshears <meeshysu@gmail.com>
2018-11-16 14:22:05 -0600

    hahah just kidding - done

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -4,7 +4,6 @@ import 'firebase/auth';
 import './auth.scss';
 
 import googleImage from './Sign-In.png';
-import allTasksArray from '../../helpers/dataGetter';
 
 const loginButton = () => {
   const domString = `
@@ -20,28 +19,6 @@ const loginButton = () => {
 };
 
 
-const showList = (task) => {
-  let domString = '';
-  domString = `
-  <div id="list" class="printedList>
-    <h4>${task.task}</h4>
-  </div>
-  `;
-  if (task.isCompleted === true) {
-    $('#lists').html(domString);
-  }
-};
-
-const initlializeLists = () => {
-  allTasksArray()
-    .then((data) => {
-      showList(data.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-
 export default {
-  loginButton, initlializeLists,
+  loginButton,
 };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import firebase from 'firebase/app';
 import apiKeys from '../db/apiKeys.json';
 
 import createNavBar from './components/Navbar/navbar';
-import login from './components/Auth/auth';
+import loginButton from './components/Auth/auth';
 import checkLoginStatus from './helpers/authHelpers';
 import getAllTasksFromDb from './helpers/dataGetter';
 
@@ -14,7 +14,7 @@ const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavBar();
   checkLoginStatus();
-  login.loginButton();
+  loginButton();
   getAllTasksFromDb();
 };
 initializeApp();
