174fbde064e6dc879db8adb6fc5bead7c51e3062
Michelle Beshears <meeshysu@gmail.com>
2018-11-16 14:19:08 -0600

    saving, trying to get list to show

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -4,6 +4,7 @@ import 'firebase/auth';
 import './auth.scss';
 
 import googleImage from './Sign-In.png';
+import allTasksArray from '../../helpers/dataGetter';
 
 const loginButton = () => {
   const domString = `
@@ -18,4 +19,29 @@ const loginButton = () => {
   });
 };
 
-export default loginButton;
+
+const showList = (task) => {
+  let domString = '';
+  domString = `
+  <div id="list" class="printedList>
+    <h4>${task.task}</h4>
+  </div>
+  `;
+  if (task.isCompleted === true) {
+    $('#lists').html(domString);
+  }
+};
+
+const initlializeLists = () => {
+  allTasksArray()
+    .then((data) => {
+      showList(data.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export default {
+  loginButton, initlializeLists,
+};
